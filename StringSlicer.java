4e074c631283060a3950f93c120520c175594999
Teddy <teddyjh@protonmail.com>
2019-11-17 19:02:48 +0100

    Add a display function to original code
    We added a display function to the original code, and put new
    method names to it all. For me: It kind of looks nicer this
    way.

---
+++ StringSlicer.java
@@ -0,0 +1,39 @@
+//package stringslicer;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class StringSlicer {
+    
+    public static ArrayList<Character> GetUserInput(){
+
+        Scanner scnr = new Scanner(System.in);
+        
+        String userInput;
+        
+        ArrayList<Character> charInput = new ArrayList<>();
+        
+        System.out.print("Enter a string of characters: ");
+        userInput = scnr.nextLine();
+        
+        for (int i = 0; i < userInput.length(); i++) {
+            charInput.add(userInput.charAt(i));
+        }
+        
+        return charInput;       
+    }
+
+    public static void DisplayUserInput(ArrayList<Character> userInput)
+    {
+        for (char input : userInput)
+        {
+            System.out.println(input);
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DisplayUserInput(GetUserInput());
+
+    }
+}
